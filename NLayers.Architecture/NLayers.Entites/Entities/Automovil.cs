ffe53c65bad7cb1703b2e033c82cc2833cbb8259
Tomas Ivan Juarez <tomasjuarez00@outlook.com>
2025-10-02 10:39:31 -0300

    Agregar clase automovil en la carpeta entities
    Se agregó la clase Automovil.cs con propiedades solicitadas: Marca, Modelo, Color, Fabricacion, NumeroMotor, NumeroChasis

---
+++ NLayers.Architecture/NLayers.Entites/Entities/Automovil.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayers.Entites.Entities
+{
+    public class Automovil
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public  required string Marca { get; set; }
+
+        [Required]
+        public required string Modelo { get; set; }
+
+        [Required]
+        public required string Color { get; set; }
+
+        [Required]
+        [Range(1900, 2100)]
+        public int Fabricacion { get; set; }
+
+        [Required]
+        public required string NumeroMotor { get; set; }
+
+        [Required]
+        public required string NumeroChasis { get; set; }
+
+    }
+}
